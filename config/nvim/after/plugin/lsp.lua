69a361fd79ba2249e9932a5cedeb744d75ea6277
julia <me@juliapaci.com>
2025-08-02 23:49:40 +1000

    julials lsp rules to instantiate the server
    tbh julia repl is good enough of an ide its like haskell ghci along side vim like idk prob dont even need this tbh

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -4,9 +4,56 @@ lsp.on_attach(function(client, bufnr)
     lsp.default_keymaps({buffer = bufnr})
 end)
 
+vim.lsp.config('julials', {
+    cmd = {
+        "julia",
+        "--project=".."~/.julia/environments/lsp/",
+        "--startup-file=no",
+        "--history-file=no",
+        "-e", [[
+            using Pkg
+            Pkg.instantiate()
+            using LanguageServer
+        depot_path = get(ENV, "JULIA_DEPOT_PATH", "")
+        project_path = let
+            dirname(something(
+                ## 1. Finds an explicitly set project (JULIA_PROJECT)
+                Base.load_path_expand((
+                    p = get(ENV, "JULIA_PROJECT", nothing);
+                        p === nothing ? nothing : isempty(p) ? nothing : p
+                    )),
+                        ## 2. Look for a Project.toml file in the current working directory,
+                        ##    or parent directories, with $HOME as an upper boundary
+                        Base.current_project(),
+                        ## 3. First entry in the load path
+                        get(Base.load_path(), 1, nothing),
+                        ## 4. Fallback to default global environment,
+                        ##    this is more or less unreachable
+                    Base.load_path_expand("@v#.#"),
+                ))
+            end
+                    @info "Running language server" VERSION pwd() project_path depot_path
+                    server = LanguageServer.LanguageServerInstance(stdin, stdout, project_path, depot_path)
+        server.runlinter = true
+            run(server)
+        ]]
+    },
+    filetypes = { 'julia' },
+    root_markers = { "Project.toml", "JuliaProject.toml" },
+    settings = {}
+})
+
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'clangd', 'rust_analyzer', 'zls'},
+  ensure_installed = {
+      'clangd',         -- c/c++
+      'rust_analyzer',  -- rust
+      'zls',            -- zig
+      'hls',            -- haskell
+      'julials',        -- julia
+      'glsl_analyzer',  -- glsl
+      'tinymist'        -- typst
+  },
   handlers = {
     lsp.default_setup,
   }
